a3124537150e39014f8a2067aa2b9ab97d1c99ef
Rob van der Does <robvddoes@gmail.com>
2021-03-09 16:29:12 +0100

    [BackUpManager] Short key_blue button label name ( cosmetic ).

--- src/BackupManager.py
+++ src/BackupManager.py
@@ -900,7 +900,7 @@ class VIXBackupManagerMenu(Setup):
 		self["key_red"] = Button(_("Cancel"))
 		self["key_green"] = Button(_("OK"))
 		self["key_yellow"] = Button(_("Choose files"))
-		self["key_blue"] = Button(_("Choose local IPK's folder"))
+		self["key_blue"] = Button(_("Choose IPK folder(s)"))
 
 	def chooseFiles(self):
 		self.session.openWithCallback(self.backupfiles_choosen, BackupSelection)
